36b0f9bb91e9fb55a6e57dcd1e3ca1834720420a
cleverpanther <cleverpanther0819@gmail.com>
2024-05-01 13:49:00 +0000

    complete

---
+++ runotis.bat
@@ -0,0 +1,3 @@
+start cmd /c runxampp.bat
+start cmd /c rundjango.bat
+start cmd /c runreact.bat
---
+++ runxampp.bat
@@ -0,0 +1,4 @@
+@echo off
+
+echo Starting XAMPP MySQL Server...
+start cmd /k "cd /D C:\xampp\mysql\bin && mysqld"
